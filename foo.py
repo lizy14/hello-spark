79ca6bac05a3f13f2a3a226466c25238f080197a
Li Zhaoyang <lizy14@yeah.net>
2017-12-13 20:20:03 +0800

    web server initial

--- foo.py
+++ foo.py
@@ -4,7 +4,9 @@ from pyspark.sql import SQLContext
 from pyspark.sql.types import StringType, DateType, IntegerType
 from pyspark import SparkConf, SparkContext
 from pyspark.sql.functions import UserDefinedFunction
+from pyspark.storagelevel import StorageLevel
 from flask import Flask, jsonify
+from utils import crossdomain
 app = Flask(__name__)
 
 conf = SparkConf() \
@@ -13,7 +15,8 @@ conf = SparkConf() \
     .set("spark.executor.memory", "1g") \
     .set("spark.ui.port", 4040)
 
-sc = SparkContext('local', conf=conf)
+sc = SparkContext(conf=conf)
+sc.addPyFile("foo.py")
 sqlContext = SQLContext(sc)
 
 def read_from_csv(table_name):
@@ -21,8 +24,12 @@ def read_from_csv(table_name):
     return sqlContext.read.csv(filename, header=True, inferSchema=True)
 
 @app.route('/购买情况/<method>/<period>')
+@crossdomain('*')
 def 购买情况(method, period):
-    
+    return _购买情况(method, period)
+
+def _购买情况(method, period):
+
     lines = read_from_csv('Purchase')
     lines = lines.select('Created', 'Currency', 'Price')
     lines = lines.withColumn('年', 
@@ -32,12 +39,18 @@ def 购买情况(method, period):
     lines = lines.withColumn('季度', 
         UserDefinedFunction(lambda d: "{yr}Q{q}".format(yr=d.year, q=(d.month + 2) // 3), StringType())('Created'))
 
-    取次数 = lambda 周期: lines.groupBy(周期).count()
-    取平均金额 = lambda 周期: lines.filter('Currency = "CN"').filter('Price > 0').groupBy(周期).avg('Price')
+    lines.persist()
+
+    取次数 = lambda 周期: lines.groupBy(周期).count().sort(周期).persist()
+    取平均金额 = lambda 周期: lines.filter('Currency = "CN"').filter('Price > 0').groupBy(周期).avg('Price').sort(周期).persist()
 
     result = (取次数 if method == '次数' else 取平均金额)(period)
-
-    return jsonify({'result': list(map(json.loads, result.toJSON().collect()))})
+    return jsonify({'result': [
+        [
+            row[period].isoformat() if period == '天' else row[period], 
+            row['count'] if method == '次数' else row['avg(Price)']
+        ] for row in result.collect()
+    ]})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
