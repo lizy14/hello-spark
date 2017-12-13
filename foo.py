86da4dff558e1ca0d326cc8248ffc20953ba0811
Li Zhaoyang <lizy14@yeah.net>
2017-12-13 17:48:53 +0800

    web server initial

--- foo.py
+++ foo.py
@@ -1,8 +1,11 @@
+import json
+
 from pyspark.sql import SQLContext
 from pyspark.sql.types import StringType, DateType, IntegerType
 from pyspark import SparkConf, SparkContext
 from pyspark.sql.functions import UserDefinedFunction
-
+from flask import Flask, jsonify
+app = Flask(__name__)
 
 conf = SparkConf() \
     .setMaster("spark://172.21.0.14:7077") \
@@ -10,28 +13,31 @@ conf = SparkConf() \
     .set("spark.executor.memory", "1g") \
     .set("spark.ui.port", 4040)
 
-sc = SparkContext(conf=conf)
+sc = SparkContext('local', conf=conf)
 sqlContext = SQLContext(sc)
 
 def read_from_csv(table_name):
     filename = "file:///home/ubuntu/DWDB/{}.csv".format(table_name)
     return sqlContext.read.csv(filename, header=True, inferSchema=True)
 
-
-
-if True: # 场景2：购买情况
-
+@app.route('/购买情况/<method>/<period>')
+def 购买情况(method, period):
+    
     lines = read_from_csv('Purchase')
+    lines = lines.select('Created', 'Currency', 'Price')
     lines = lines.withColumn('年', 
         UserDefinedFunction(lambda x: x.year, IntegerType())('Created'))
     lines = lines.withColumn('天', 
-        UserDefinedFunction(lambda x: x.year, DateType())('Created'))
+        UserDefinedFunction(lambda x: x.date(), DateType())('Created'))
     lines = lines.withColumn('季度', 
         UserDefinedFunction(lambda d: "{yr}Q{q}".format(yr=d.year, q=(d.month + 2) // 3), StringType())('Created'))
 
+    取次数 = lambda 周期: lines.groupBy(周期).count()
+    取平均金额 = lambda 周期: lines.filter('Currency = "CN"').filter('Price > 0').groupBy(周期).avg('Price')
 
-    取次数 = lambda p: lines.groupBy(p).count()
-    取平均金额 = lambda p: lines.filter('Currency = "CN"').filter('Price > 0').groupBy(p).avg('Price')
+    result = (取次数 if method == '次数' else 取平均金额)(period)
 
-    print(取次数('季度'))   # DataFrame[季度: string, count: bigint]
-    print(取平均金额('年')) # DataFrame[年: int, avg(Price): double]
+    return jsonify({'result': list(map(json.loads, result.toJSON().collect()))})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
